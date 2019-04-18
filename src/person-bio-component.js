ce039cf7256b19ae0417bf976d0a321387697954
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-18 08:10:39 -0700

    Mobile version of about.html

---
+++ src/person-bio-component.js
@@ -0,0 +1,26 @@
+class Person {
+  constructor(person) {
+    this.person = person;
+    this.bioimage = this.person.querySelector(".person-image");
+    this.info = this.person.querySelector(".person-text");
+    this.bioimage.addEventListener("click", () => this.select());
+  }
+
+  select() {
+    const text = document.querySelectorAll(".person-text");
+    Array.from(text).forEach(item => {
+      item.classList.add("hidden");
+    });
+
+    this.info.classList.remove("hidden");
+  }
+}
+
+const personCards = document.querySelectorAll(".person-card");
+
+personCards.forEach(item => {
+  //   console.log(item);
+  return new Person(item);
+});
+
+// console.log(personCards);
